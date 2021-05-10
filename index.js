18796a948c388bcba7207bf3d79ed0a085d607fe
sheenamnarula <sheenam.narula@trantorinc.com>
2021-05-10 22:50:37 +0530

    date filter added

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ const server = async () => {
     const csv = require("csvtojson");
     let jsonArray = await csv().fromFile(csvFilePath);
 
-    let jsonData = jsonArray.map((elem, i) => {
-      return { lat: parseFloat(elem.lat), lng: parseFloat(elem.long) };
-    });
     jsonArray = JSON.stringify(jsonArray);
-    let start = JSON.stringify(jsonData[0]);
-    jsonData.shift();
-    jsonData = JSON.stringify(jsonData);
     res.setHeader("content-type", "text/html");
 
     res.status(200).send(
@@ -27,6 +21,8 @@ const server = async () => {
 <html>
   <head>
     <title>Route Display</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 
     <style type="text/css">
@@ -43,70 +39,84 @@ const server = async () => {
     </style>
     <script type="text/javascript">
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: 
-     ${start},
-    zoom: 6,
-    mapTypeId: "terrain",
-  });
-  
-  const lineSymbol = {
-    path: google.maps.SymbolPath.CIRCLE,
-    scale: 8,
-    strokeColor: "#393",
-  };
-  let data = ${jsonArray}
-  let latLngArr = []
-  data.map((elem)=>{
-      
-      if(elem.createdAt.includes('12/1/2020')){
-          latLngArr.push({lat : parseFloat(elem.lat), lng: parseFloat(elem.long)}) 
+    let data = ${jsonArray}
+
+    let start = {}
+
+    function handleOnChange(dateElem){
+      let selectedDate;
+      if(!dateElem){
+        selectedDate = data[0].createdAt.split(" ")[0]
+      }else {
+      selectedDate = dateElem.value ;
       }
-  })
-  console.log(latLngArr)
+      let latLngArr =[]
+      data.map((elem)=>{
+          
+          if(elem.createdAt.includes(selectedDate)){
+              latLngArr.push({lat : parseFloat(elem.lat), lng: parseFloat(elem.long)}) 
+          }
+      })
+       start = latLngArr.shift()
+      initMap(latLngArr,start)
+    }
 
-  const line = new google.maps.Polyline({
-    path: latLngArr,
-    icons: [
-      {
-        icon: lineSymbol,
-        offset: "100%",
-      },
-    ],
-    map: map,
-  });
-  animateCircle(line);
-}
+    function initMap(latLngArr,start) {
+    let datesPushed = []
+    for (i = 0; i < data.length; i++) {
+      if(!datesPushed.includes(data[i].createdAt.split(" ")[0])){
+        datesPushed.push(data[i].createdAt.split(" ")[0])
+        $("#selectId").append("<option>" + data[i].createdAt.split(" ")[0] + "</option>");
+      }
+    }
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: 
+      start,
+      zoom: 6,
+      mapTypeId: "terrain",
+    });
+  
+    const lineSymbol = {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 8,
+      strokeColor: "#393",
+    };
+    
+    const line = new google.maps.Polyline({
+      path: latLngArr,
+      icons: [
+        {
+          icon: lineSymbol,
+          offset: "100%",
+        },
+      ],
+      map: map,
+    });
+    animateCircle(line);
+    latLngArr = []
+  }
 
-function animateCircle(line) {
-  let count = 0;
-  window.setInterval(() => {
-    count = (count + 1) % 200;
-    const icons = line.get("icons");
-    icons[0].offset = count / 2 + "%";
-    line.set("icons", icons);
-  }, 300);
-}
+  function animateCircle(line) {
+    let count = 0;
+    window.setInterval(() => {
+      count = (count + 1) % 200;
+      const icons = line.get("icons");
+      icons[0].offset = count / 2 + "%";
+      line.set("icons", icons);
+    }, 300);
+  }
+
     </script>
   </head>
   <body style="height:100%;margin:0;padding:0;">
-  <form action="/action_page.php">
   <label for="dates">Select date for route:</label>
-  <select>
-  <option value="12-13-2021">12-13-2021</option>
-  <option value="12-13-2021">12-13-2021</option>
-  <option value="12-13-2021">12-13-2021</option>
-
-  </select>
+  <select id=selectId onchange="handleOnChange(this)"></select>
   <br><br>
-  <input type="submit" value="Submit">
-</form>
     <div style="height:100%;" id="map"> 
     </div>
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTy5nM522z31mZc-mChFN-UtMNVwJeNZw&callback=initMap&libraries=&v=weekly"
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTy5nM522z31mZc-mChFN-UtMNVwJeNZw&callback=handleOnChange&libraries=&v=weekly"
     async
     ></script>
   </body>
